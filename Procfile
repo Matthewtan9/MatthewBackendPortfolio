a1aa04998e01aca5e527f9eb0fda4c6fb6467d77
Matthewtan9 <matthew999099@gmail.com>
2024-02-09 17:12:52 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar backendportfolio-0.0.1-SNAPSHOT-plain.jar
---
+++ src/main/java/matthew/portfolio/Classes/DataLayer/Semester.java
@@ -0,0 +1,10 @@
+package matthew.portfolio.Classes.DataLayer;
+
+public enum Semester {
+    SEMESTER_1,
+    SEMESTER_2,
+    SEMESTER_3,
+    SEMESTER_4,
+    SEMESTER_5,
+    SEMESTER_6,
+}
