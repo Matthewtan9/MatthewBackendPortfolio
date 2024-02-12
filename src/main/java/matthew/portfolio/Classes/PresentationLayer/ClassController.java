35edac607e2185b5074d441721ae7a5e8155082b
Matthewtan9 <matthew999099@gmail.com>
2024-02-11 22:52:36 -0500

    officially deployed git add .git add .git add .

--- src/main/java/matthew/portfolio/Classes/PresentationLayer/ClassController.java
+++ src/main/java/matthew/portfolio/Classes/PresentationLayer/ClassController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://matthew-portfolio-75oxyqfon-matthewtan9s-projects.vercel.app/"},allowCredentials = "true", allowedHeaders = {"xsrf-token", "content-type"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://matthew-portfolio-75oxyqfon-matthewtan9s-projects.vercel.app/","matthewtan-portfolio.vercel.app"},allowCredentials = "true", allowedHeaders = {"xsrf-token", "content-type"})
 @RequestMapping("classes")
 public class ClassController {
 
